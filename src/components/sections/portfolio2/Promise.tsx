24395b6034a28b52b794b85f094dc145c431ada2
siddharthanv <siddharthanv1205@gmail.com>
2025-12-28 22:53:38 +0530

    added more projects into creative design 1

--- src/components/sections/portfolio2/Promise.tsx
+++ src/components/sections/portfolio2/Promise.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Box, Container, Typography, Divider } from "@mui/material";
 
 const Promise = () => {
--- src/components/sections/portfolio2/WelcomeSection.tsx
+++ src/components/sections/portfolio2/WelcomeSection.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Box, Container, Typography } from "@mui/material";
 
 const PortfolioWelcome = () => {
